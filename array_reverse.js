a4d0357f0c392ab5e07bfb627e2da65753878287
Mike K <m.kermes89@gmail.com>
2018-10-03 18:19:53 -0700

    working solution

--- array_reverse.js
+++ array_reverse.js
@@ -1,17 +1,17 @@
-var base = [1, 2, 3, 4, 5];
+const base = [1, 2, 3, 4];
 let rev = [];
-const Reverse = (base) => {
-  let rev = [];
-var base = [1, 2, 3, 4, 5];
-  for (let i = 0; i < base.length; i++){
-    const Last = (base) => {
-      return base[base.length - 1];
-    }
-    let lastEle = Last(base);
+
+const reverse = () => {
+  for(let i = 0; i <= base.length -1 ; i++){
+    let lastEle = base[base.length - i -1];
     rev[rev.length] = lastEle;
+    //console.log(base[base.length - i]);
+    //console.log(rev);
+    //console.log(lastEle);
   }
   return rev;
-}
+};
 
-Reverse(base);
+console.log(base);
 console.log(rev);
+reverse(base);
